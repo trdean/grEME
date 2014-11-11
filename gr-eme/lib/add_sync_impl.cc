780ae70a504fd5441b7e349c8ea048f19f4cc390
trdean <deanthomas860@gmail.com>
2014-11-11 11:14:43 -0500

    Adds shell blocks for modulation

---
+++ gr-eme/lib/add_sync_impl.cc
@@ -0,0 +1,80 @@
+/* -*- c++ -*- */
+/* 
+ * Copyright 2014 <+YOU OR YOUR COMPANY+>.
+ * 
+ * This is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3, or (at your option)
+ * any later version.
+ * 
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this software; see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street,
+ * Boston, MA 02110-1301, USA.
+ */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <gnuradio/io_signature.h>
+#include "add_sync_impl.h"
+
+namespace gr {
+  namespace eme {
+
+    add_sync::sptr
+    add_sync::make()
+    {
+      return gnuradio::get_initial_sptr
+        (new add_sync_impl());
+    }
+
+    /*
+     * The private constructor
+     */
+    add_sync_impl::add_sync_impl()
+      : gr::block("add_sync",
+              gr::io_signature::make(<+MIN_IN+>, <+MAX_IN+>, sizeof(<+ITYPE+>)),
+              gr::io_signature::make(<+MIN_OUT+>, <+MAX_OUT+>, sizeof(<+OTYPE+>)))
+    {}
+
+    /*
+     * Our virtual destructor.
+     */
+    add_sync_impl::~add_sync_impl()
+    {
+    }
+
+    void
+    add_sync_impl::forecast (int noutput_items, gr_vector_int &ninput_items_required)
+    {
+        /* <+forecast+> e.g. ninput_items_required[0] = noutput_items */
+    }
+
+    int
+    add_sync_impl::general_work (int noutput_items,
+                       gr_vector_int &ninput_items,
+                       gr_vector_const_void_star &input_items,
+                       gr_vector_void_star &output_items)
+    {
+        const <+ITYPE*> *in = (const <+ITYPE*> *) input_items[0];
+        <+OTYPE*> *out = (<+OTYPE*> *) output_items[0];
+
+        // Do <+signal processing+>
+        // Tell runtime system how many input items we consumed on
+        // each input stream.
+        consume_each (noutput_items);
+
+        // Tell runtime system how many output items we produced.
+        return noutput_items;
+    }
+
+  } /* namespace eme */
+} /* namespace gr */
+
--- gr-eme/swig/eme_swig.i
+++ gr-eme/swig/eme_swig.i
@@ -8,6 +8,21 @@
 %include "eme_swig_doc.i"
 
 %{
+#include "eme/rs_encode_cc.h"
+#include "eme/interleave.h"
+#include "eme/graycode.h"
+#include "eme/add_sync.h"
+#include "eme/fsk_modulate.h"
 %}
 
 
+%include "eme/rs_encode_cc.h"
+GR_SWIG_BLOCK_MAGIC2(eme, rs_encode_cc);
+%include "eme/interleave.h"
+GR_SWIG_BLOCK_MAGIC2(eme, interleave);
+%include "eme/graycode.h"
+GR_SWIG_BLOCK_MAGIC2(eme, graycode);
+%include "eme/add_sync.h"
+GR_SWIG_BLOCK_MAGIC2(eme, add_sync);
+%include "eme/fsk_modulate.h"
+GR_SWIG_BLOCK_MAGIC2(eme, fsk_modulate);
